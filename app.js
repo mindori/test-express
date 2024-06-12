518e958bf7dd53a1d127ef7d0dd2eeae828caa6f
mindori <realmindori@gmail.com>
2024-06-12 17:30:09 +0900

    fix codes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Mindori World");
+  res.send("Hello Mindori New World");
 });
 
 app.get("/healthz", (req, res) => {
