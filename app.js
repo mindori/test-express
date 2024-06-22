e3a9c7a4b939672eb275f17a43e944472eec93c6
mindori <realmindori@gmail.com>
2024-06-22 20:09:37 +0900

    fix codes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Mindori New World");
+app.get("/", async (req, res) => {
+  const response = await axios.get(`https://www.naver.com`);
+  res.send(response);
 });
 
 app.get("/healthz", (req, res) => {
